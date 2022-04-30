04b9f897d85c47080b060710c0ddfc9987ebb552
lithium <code@hlidskialf.com>
2022-04-30 08:29:22 -0700

    wsgi needs src in sys.path

--- wsgi.py
+++ wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+
+BASE_DIR = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(BASE_DIR, 'src'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mainsite.settings')
 
